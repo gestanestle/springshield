90d051db53e3b2ef9e115ee66314e03ff56248ef
gestanestle <gesta.nestleo@gmail.com>
2022-11-12 02:20:49 +0800

    test: add repository tests

---
+++ src/test/java/com/krimo/BackendService/user/entity/repository/UserRepoTest.java
@@ -0,0 +1,45 @@
+package com.krimo.BackendService.user.entity.repository;
+
+import com.krimo.BackendService.user.entity.model.User;
+import com.krimo.BackendService.user.entity.model.UserRole;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepoTest {
+
+    @Autowired
+    private UserRepo repoTest;
+
+    @Test
+    void findByEmail() {
+
+        // Given
+        String email = "swift.taylor@gmail.com";
+        User user =  new User (
+                email,
+                "password",
+                "Swift",
+                "Taylor",
+                "Alison",
+                LocalDate.of(1989, Month.DECEMBER, 13),
+                UserRole.USER
+        );
+
+        // when
+        repoTest.save(user);
+
+        // Then
+        boolean expected = repoTest.findByEmail(email).isPresent();
+        assertThat(expected).isTrue();
+
+    }
+}
